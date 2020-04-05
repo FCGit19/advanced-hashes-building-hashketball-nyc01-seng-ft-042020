93d98a2a0fee53af4806ac9a77e0e26f8f44a9db
Fred C <canofred@gmail.com>
2020-04-05 16:55:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,17 +2,11 @@ def game_hash
   qualities = {
     
     home: { 
-      team_name: "Brooklyn Nets" }, 
+      team_name: "Brooklyn Nets", 
     
-    { :colors = 
+    colors: [ "Black", "White" ], 
     
-    [ "Black", "White" ] 
-      
-    }, 
-    
-    { :players => 
-    
-    [ 
+    players: [ 
       
       {:player_name => "Alan Anderson"}, {:number => 0}, {:shoe => 16}, {:points => 22}, {:rebounds => 12}, {:assists => 12}, {:steals => 3}, {:blocks => 1}, {:slam_dunks => 1} 
       
