01007be5d50ee0bea15e6d851242473fd554686e
Fred C <canofred@gmail.com>
2020-04-05 15:54:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,11 @@
 def game_hash
-  qualities = { :home => {:team_name => , :colors => , :players => }, :away => {:team_name => , :colors => , :players => }
+  qualities = { :home => 
+  
+  { :team_name => "team name"}, { :colors => ["team colors"] }, { :players => [ { :player_name => }, { :number => }, { :shoe => }, { :points => }, { :rebounds => }, {:assists => { :steals => }, { :blocks => }, { :slam_dunks => } } ]}, 
+  
+  :away => 
+  
+  { :team_name => }, { :colors => }, { :players => }
   
   qualities
 end 
