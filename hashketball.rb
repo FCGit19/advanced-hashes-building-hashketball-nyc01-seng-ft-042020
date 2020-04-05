84566d236d982a5d4378ccb7d96c5abd0fc9a4d7
Fred C <canofred@gmail.com>
2020-04-05 16:57:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,18 +4,26 @@ def game_hash
     home: { 
       team_name: "Brooklyn Nets", 
     
-    colors: [ "Black", "White" ], 
+      colors: [ "Black", "White" ], 
     
-    players: [ 
+      players: [ 
       
-      {:player_name => "Alan Anderson"}, {:number => 0}, {:shoe => 16}, {:points => 22}, {:rebounds => 12}, {:assists => 12}, {:steals => 3}, {:blocks => 1}, {:slam_dunks => 1} 
+        {:player_name => "Alan Anderson"}, {:number => 0}, {:shoe => 16}, {:points => 22}, {:rebounds => 12}, {:assists => 12}, {:steals => 3}, {:blocks => 1}, {:slam_dunks => 1} 
       
       ] 
+    },
+    
+    away: { 
+      team_name: "Brooklyn Nets", 
+    
+      colors: [ "Black", "White" ], 
+    
+      players: [ 
       
+        {:player_name => "Alan Anderson"}, {:number => 0}, {:shoe => 16}, {:points => 22}, {:rebounds => 12}, {:assists => 12}, {:steals => 3}, {:blocks => 1}, {:slam_dunks => 1} 
+      
+      ] 
     }
-    
-    
-    :away => {:team_name => "team name"}, {:colors = ["colors"]}, {:players => [ {:player_name => }, {:number => }, {:shoe => }, {:points => }, {:rebounds => }, {:assists => }, {:steals => }, {:blocks => }, {:slam_dunks => } ]}
   }
 
   
